5d653822d1f4e5f5c473af8104dc168126d4e519
TristenA <tristenaguilar24@gmail.com>
2017-10-30 19:26:57 -0400

    Uploaded App

---
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -0,0 +1,149 @@
+package com.londonappbrewery.quizzler;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // TODO: Declare member variables here:
+    Button mTrueButton;
+    Button mFalseButton;
+    TextView mQuestionTextView;
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
+    int mScore;
+    int mIndex;
+    int mQuestion;
+
+    //TODO: Uncomment to create question bank
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
+
+
+    // TODO: Declare constants here
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0/mQuestionBank.length); //100 is the length of progress bar.
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if(savedInstanceState != null){
+            mScore = savedInstanceState.getInt("ScoreKey");
+            mIndex = savedInstanceState.getInt("IndexKey");
+        }else{
+            mScore = 0;
+            mIndex = 0;
+        }
+
+
+    mTrueButton = (Button) findViewById(R.id.true_button);
+    mFalseButton = (Button) findViewById(R.id.false_button);
+    mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+    mScoreTextView = (TextView)findViewById(R.id.score);
+    mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
+
+    mScoreTextView.setText("Score: " + mScore + "/" + mQuestionBank.length); //This is put here to update the score after rotation
+
+    //TrueFalse firstQuestion = mQuestionBank[mIndex];
+    mQuestion = mQuestionBank[mIndex].getQuestionID();
+    mQuestionTextView.setText(mQuestion);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
+
+                //Log.d("Quizzler","Clicked!");
+                //Toast.makeText(getApplicationContext(),"True Pressed!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                checkAnswer(false);
+                updateQuestion();
+
+                //Toast myToast = Toast.makeText(getApplicationContext(),"False Pressed!",Toast.LENGTH_SHORT);
+                //myToast.show();
+            }
+        });
+
+          //Type    //Name                //Calling Constructor
+        //TrueFalse exampleQuestion = new TrueFalse(R.string.question_1, true); <--     Example
+                            //You can use the R class to access any element by its type
+
+    //You can set an onClick listener for anything that is a view.
+
+    }
+
+    private void updateQuestion(){
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+
+        if(mIndex == 0){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this); //'this' refers to the mainActivity
+            alert.setTitle("Game Over");
+            alert.setCancelable(false);
+            alert.setMessage("You scored " + mScore + " points");
+            alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        mScoreTextView.setText("Score: " + mScore + "/" + mQuestionBank.length);
+    }
+
+    private void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
+
+        if(userSelection == correctAnswer){
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            mScore = mScore + 1;
+        }
+        else{
+            Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){ //outState stores the state of our app when the main activity is being restarted due to the screen rotation
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("ScoreKey",mScore);
+        outState.putInt("IndexKey",mIndex);
+    }
+
+}
